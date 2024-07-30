eeda20b19332e09865cde28b125f43a0fad6deef
wangjiawen <jarvanw1111@gmail.com>
2024-07-30 17:10:06 +0800

    commit OpenAI聊天和生成图片功能前后端互通

---
+++ src/main/java/org/spring/aicloud/controller/OpenAIController.java
@@ -0,0 +1,58 @@
+package org.spring.aicloud.controller;
+
+import jakarta.annotation.Resource;
+import org.spring.aicloud.util.ResponseEntity;
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.image.ImageResponse;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiImageModel;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: JarvanW
+ * @Date: 2024/7/30
+ * @Description:
+ * @Requirements:
+ */
+
+
+@RestController
+@RequestMapping("/openai")
+public class OpenAIController {
+
+    @Resource
+    private OpenAiChatModel chatModel;
+    @Resource
+    private OpenAiImageModel imageModel;
+
+
+    /**
+     * 调用 Open AI 聊天接口
+     *
+     * @param question
+     * @return
+     */
+    @RequestMapping("/chat")
+    public ResponseEntity chat(String question) {
+        if (!StringUtils.hasLength(question)) {
+            // 输入为空
+            return ResponseEntity.error("请先输入内容!");
+        }
+        // 调用 Open AI 接口
+        String result = chatModel.call(question);
+        return ResponseEntity.success(result);
+    }
+
+    @RequestMapping("/draw")
+    public ResponseEntity draw(String question) {
+        if (!StringUtils.hasLength(question)) {
+            // 输入为空
+            return ResponseEntity.error("请先输入内容!");
+        }
+        // 调用 Open AI 接口
+        ImageResponse result = imageModel.call(new ImagePrompt(question));
+        return ResponseEntity.success(result.getResults().get(0));
+    }
+}
---
+++ src/main/java/org/spring/aicloud/util/ResponseEntity.java
@@ -0,0 +1,40 @@
+package org.spring.aicloud.util;
+
+/**
+ * @Author: JarvanW
+ * @Date: 2024/7/30
+ * @Description:
+ * @Requirements:
+ */
+
+
+import lombok.Data;
+
+/**
+ * 统一返回对象 （进行 Ajax 响应）
+ */
+@Data
+public class ResponseEntity {
+
+    private int code;   // 状态码
+    private String msg;    // 状态信息
+    private Object data;    // 数据
+
+
+    public static ResponseEntity success(Object data) {
+        ResponseEntity responseEntity = new ResponseEntity();
+        responseEntity.setCode(200);
+        responseEntity.setMsg("success");
+        responseEntity.setData(data);
+        return responseEntity;
+    }
+
+    public static ResponseEntity error(String msg) {
+        ResponseEntity responseEntity = new ResponseEntity();
+        responseEntity.setCode(500);
+        responseEntity.setMsg(msg);
+        responseEntity.setData(null);
+        return responseEntity;
+    }
+
+}
