cb56753a6d336c8789056c7d0139a7d371610334
wangjiawen <jarvanw1111@gmail.com>
2024-07-31 21:54:07 +0800

    commit 添加Redis实现动态验证码判断

--- src/main/java/org/spring/aicloud/controller/CaptchaController.java
+++ src/main/java/org/spring/aicloud/controller/CaptchaController.java
@@ -3,10 +3,14 @@ package org.spring.aicloud.controller;
 import cn.hutool.captcha.CaptchaUtil;
 import cn.hutool.captcha.LineCaptcha;
 import cn.hutool.core.net.NetUtil;
+import cn.hutool.crypto.SecureUtil;
+import cn.hutool.crypto.digest.HMac;
 import io.minio.errors.*;
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
 import org.spring.aicloud.util.MinIoUtil;
 import org.spring.aicloud.util.ResponseEntity;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +19,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author: JarvanW
@@ -30,17 +35,46 @@ public class CaptchaController {
     @Resource
     private MinIoUtil minIoUtil;
 
+    @Resource
+    private RedisTemplate redisTemplate;
+
     /**
      * 生成验证码
      */
     @RequestMapping("/create")
-    public ResponseEntity createCaptcha() throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+    public ResponseEntity createCaptcha(HttpServletRequest request) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+
+//        // 操作 String 类型
+//        redisTemplate.opsForValue().set("key", "value");      // 存
+//        redisTemplate.opsForValue().get("key");     // 取
+//
+//
+//        // 操作 Hash
+//        redisTemplate.opsForHash().put("hashKey", "hashField", "hashValue");
+//        redisTemplate.opsForHash().get("hashKey", "hashField");
+//
+//
+//        // 操作 List
+//        redisTemplate.opsForList().leftPush("listKey", "listValue");
+//        redisTemplate.opsForList().rightPop("listKey");
+//
+//
+//        // 操作 Set
+//        redisTemplate.opsForSet().add("setKey", "setValue");
+//        redisTemplate.opsForSet().members("setKey");
+//
+//        // 操作 Zset
+//        redisTemplate.opsForZSet().add("zsetKey", "zsetValue", 1.0);
+//        redisTemplate.opsForZSet().range("zsetKey", 0, -1);
+
         String url = "";
         // 定义图形验证码的长和宽
         LineCaptcha lineCaptcha = CaptchaUtil.createLineCaptcha(120, 40);
-        String fileName = "captcha-" + NetUtil.getLocalhost().toString();
+        String fileName = "captcha-" + SecureUtil.md5(request.getRemoteAddr());
         try (InputStream inputStream = new ByteArrayInputStream(lineCaptcha.getImageBytes())) {
             url = minIoUtil.upload(fileName, inputStream, "image/png");
+            String code = lineCaptcha.getCode();      // 正确的验证码
+            redisTemplate.opsForValue().set(fileName, code,60, TimeUnit.SECONDS);   // 验证码存储到Redis中
         }
         return ResponseEntity.success(url);
     }
--- src/main/java/org/spring/aicloud/controller/UserController.java
+++ src/main/java/org/spring/aicloud/controller/UserController.java
@@ -1,6 +1,10 @@
 package org.spring.aicloud.controller;
 
+import cn.hutool.crypto.SecureUtil;
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
 import org.spring.aicloud.util.ResponseEntity;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,15 +20,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
     // 登录 login
     @RequestMapping("/login")
-    public ResponseEntity login(String username, String password) {
+    public ResponseEntity login(String username, String password, String captcha, HttpServletRequest request) {
         // 1. 非空判断
-        if (!StringUtils.hasLength(username)|| !StringUtils.hasLength(password)){
-            return ResponseEntity.error("用户名或密码不能为空");
+        if (!StringUtils.hasLength(username) || !StringUtils.hasLength(password) || !StringUtils.hasLength(captcha)) {
+            return ResponseEntity.error("参数有误！");
         }
 
-        if ("admin".equals(username) && "admin".equals(password)){
+        // 2. 验证图片验证码
+        String redisCaptchaKey = "captcha-" + SecureUtil.md5(request.getRemoteAddr());
+        String redisCaptcha = (String) redisTemplate.opsForValue().get(redisCaptchaKey);
+        if (!StringUtils.hasLength(redisCaptcha) || !redisCaptcha.equalsIgnoreCase(captcha)) {
+            return ResponseEntity.error("输入验证码错误，请重新输入！");
+        }
+
+        // 3. 验证用户名密码
+        if ("admin".equals(username) && "admin".equals(password)) {
             return ResponseEntity.success("登录成功");
         }
         return ResponseEntity.error("用户名或密码不正确");
